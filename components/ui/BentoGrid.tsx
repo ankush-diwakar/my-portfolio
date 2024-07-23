4dc6ec4df94645675607bcd463827f01fc8c3701
ankush-diwakar <150348342+ankush-diwakar@users.noreply.github.com>
2024-07-23 20:13:01 +0530

    added some modifications in BENTO GRID

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -46,8 +46,8 @@ export const BentoGridItem = ({
     titleClassName?: string;
     spareImg?: string;
 }) => {
-    const leftLists = ["ReactJS", "Express", "Typescript"];
-    const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+    const leftLists = ["ReactJS", "Express JS", "Typescript"];
+    const rightLists = ["MongoDB", "MySQL", "NextJS"];
 
     const [copied, setCopied] = useState(false);
 
